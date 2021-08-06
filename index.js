64d2b2a731f3f4f3becdb1f58f90ba74cc34c0c7
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2021-08-06 02:55:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Install the dependencies
+var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
+var express = require('express');
+var app = express();
+
+// Listen on the root domain (/) and display something
+app.get('/', (req, res) => {
+  res.send(`<br> Media proxy -> <strong style="color: lightgreen; font-family: Arial;">online<strong>
+	<br>
+	<p>Chocolate Front Cam:</p>
+	<img src="/chocolate-front" width="100"></img> <br>
+	<p>Claw Front Cam:</p>
+	<img src="/toy-front" width="100"></img> <br>
+	`);
+})
+
+// Chocolate Machine Cameras
+app.get('/chocolate-front', new MjpegProxy('http://ramsom.ddns.net:8082/video').proxyRequest);
+
+// Toy Claw Machine Cameras
+app.get('/toy-front', new MjpegProxy('http://ramsom.ddns.net:8081/video').proxyRequest);
+
+// Start the server on port 80
+app.listen(80);
