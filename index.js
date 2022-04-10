8757df3b0a7ebf366e3c878e36749d85487cb8ac
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2022-04-10 14:09:56 +1000

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var ports = [80, 8080, 443];
 ports.forEach(function(port) {
   // Listen on the root domain (/) and display something
   app.get('/', (req, res) => {
-    res.send(`<br> working`);
+    res.send(`<h1>Netclaw Proxy</h1> <br> <h5>v0.1.1 - 10-04-2022, 14:09</h5>`);
   });
 
   // Toy Soldier Camera
