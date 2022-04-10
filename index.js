a70a5c23c75b0c05fcfbcd4746df98bd0048c1a9
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2022-04-10 13:58:09 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
 var express = require('express');
 var app = express();
 
+var express2 = require('express');
+var app2 = express2();
+
 // Listen on the root domain (/) and display something
 app.get('/', (req, res) => {
   res.send(`<br> working`);
@@ -13,3 +16,10 @@ app.get('/toy-soldier', new MjpegProxy('http://netclaw.ddns.net:8081/video').pro
 
 // Start the server on port 80
 app.listen(80);
+
+// Listen on port 8080
+app2.get('/', (req, res) => {
+  res.send(`<br> working`);
+});
+
+app2.listen(8080);
