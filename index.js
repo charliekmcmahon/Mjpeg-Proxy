8897903b2816be7a0a731787691b6e49abdaccc2
Charlie McMahon <charliekmcmahon@gmail.com>
2022-05-28 13:19:46 +1000

    Add support for MaxiClaw and video stream 2

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ ports.forEach(function(port) {
   // Toy Soldier Camera
   app.get('/toy-soldier', new MjpegProxy('http://netclaw.ddns.net:8080/video').proxyRequest);
   
-  // Test camera
+  // Test camera for beta testing
   app.get('/video/1', new MjpegProxy('http://netclaw.ddns.net:8080/video').proxyRequest);
+  
+  app.get('/video/2', new MjpegProxy('http://netclaw.ddns.net:8080/video').proxyRequest);
 
   // Start the servers on each port
   app.listen(port);
