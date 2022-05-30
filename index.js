bf5dd174fd5a5abf8d6254227b2e90bb479524d6
Charlie McMahon <charliekmcmahon@gmail.com>
2022-05-30 19:45:44 +1000

    disable the proxy

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ var ports = [80, 443, 8080];
 ports.forEach(function(port) {
   // Listen on the root domain (/) and display something
   app.get('/', (req, res) => {
-    res.send(`<h1>Netclaw Proxy</h1> <br> <h5>v0.1.1 - 10-04-2022, 14:09</h5>`);
+    res.send(`<h1>Netclaw Proxy</h1>`);
   });
 
   // Toy Soldier Camera
   app.get('/toy-soldier', new MjpegProxy('http://netclaw.ddns.net:8080/video').proxyRequest);
   
   // Test camera for beta testing
-  app.get('/video/1', new MjpegProxy('http://netclaw.ddns.net:8080/video').proxyRequest);
+  //app.get('/video/1', new MjpegProxy('http://netclaw.ddns.net:8080/video').proxyRequest);
   
-  app.get('/video/2', new MjpegProxy('http://netclaw.ddns.net:8080/video').proxyRequest);
+  //app.get('/video/2', new MjpegProxy('http://netclaw.ddns.net:8080/video').proxyRequest);
 
   // Start the servers on each port
   app.listen(port);
