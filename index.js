d3ad3822051decd0ff8e26cd74e0a29deec199a6
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2024-09-28 15:43:49 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,17 @@
 var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
 var express = require('express');
 var app = express();
-var ports = [80, 443, 8080];
+var ports = [80, 443, 8080]; // Ports to run the proxy on
 
 ports.forEach(function(port) {
   // Listen on the root domain (/) and display something
   app.get('/proxy', (req, res) => {
-    res.send(`<h1>Netclaw Proxy</h1> <br> <h5>v0.1.1 - 10-04-2022, 14:09</h5>`);
+    res.send(`<h1>mjpeg proxy</h1> <br> <h5>Server is OK</h5>`);
   });
 
-  // Toy Soldier Camera
-  app.get('/proxy/toy-soldier', new MjpegProxy('http://netclaw.ddns.net:8080/video').proxyRequest);
+  // Create a proxy
+  app.get('/proxy/insert-name-here', new MjpegProxy('http://mjpeg-stream-location:8080/video').proxyRequest);
   
-  // Test camera
-  app.get('/proxy/video/1', new MjpegProxy('http://49.191.169.230:4747/video?640x480').proxyRequest);
-
   // Start the servers on each port
   app.listen(port);
   
